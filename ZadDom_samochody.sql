fd6e40f1408eb30203abafc9bf9e26fb10c5177e
DESKTOP-231T7BN\Maciek <m.bandzarzewski>
2017-09-13 01:41:17 +0200

    aktualizacja

--- ZadDom_samochody.sql
+++ ZadDom_samochody.sql
@@ -7,3 +7,8 @@ SELECT * FROM auta WHERE rocznik=2010;
 SELECT *FROM auta WHERE marka='Ford';
 #4.Samochody o numerach w bazie od 2 do 4 włącznie.
 SELECT*FROM auta WHERE idauta >=2 AND idauta<=4;
+#5.Imiona i nazwiska klientów, którzy mieszkają w Katowicach na ulicy Rolnej (pod dowolnym numerem).
+SELECT imie,nazwisko FROM klienci WHERE adres LIKE '%Rolna%';
+#6.Samochód w bazie (marka, model), którego ubezpieczenie jest najdroższe z wszystkich aut.
+SELECT marka,model,ubezpieczenie FROM auta WHERE ubezpieczenie = (SELECT MAX(ubezpieczenie) FROM auta);
+#7.Samochód (idauta, marka, model), który został najwcześniej wypożyczony (decyduje datawyp).
