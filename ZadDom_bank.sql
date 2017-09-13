cf180fef21b3a47a5a54d12bc007d8e24b37124a
DESKTOP-231T7BN\Maciek <m.bandzarzewski>
2017-09-13 16:06:43 +0200

    aktualizacja

--- ZadDom_bank.sql
+++ ZadDom_bank.sql
@@ -11,4 +11,20 @@ SELECT imie,nazwisko,pensja FROM pracownicy;
 SELECT imie,nazwisko, pensja+dodatki FROM pracownicy;
 # 6. Zmodyfikuj tak zapytanie z zadania nr 5 aby nadać nową nazwę „wynagrodzenie całkowite” kolumnie o nazwie „No column name”.
 SELECT imie,nazwisko, (pensja+dodatki)AS'wynagrodzenie całkowite' FROM  pracownicy;
+#7. Oblicz ile dany pracownik zarabia dziennie, tygodniowo, miesięcznie i rocznie. Wynik posortuj malejąco. 
+# Dane wyświetl w konwencji: Imię i Nazwisko, Dniówka, Tygodniówka, Pensja miesięczna, Pensja roczna.
+SELECT 
+    imie,
+    nazwisko,
+    (pensja * 12) AS 'Roczna pensja',
+    pensja AS 'Miesieczna pensja',
+    ROUND(pensja / 4, 2) AS 'Tygodniowa pensja',
+    ROUND(pensja / 30, 2) AS 'Dzienne zarobki'
+FROM
+    pracownicy;
+
+# 8. Pokaż wszystkich pracowników, których nazwiska zaczynają się na M. Wyświetl tylko Imię i Nazwisko pracownika. 
+SELECT imie, nazwisko FROM pracownicy WHERE nazwisko LIKE 'M%';
+# 9. 
+
 
