89ba77427a96c1b233abd268995e1139d971d9d3
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-09-14 20:54:26 +0200

    Aktualzacja bazy danych

--- Dzien5_baza_przychodniaZdrowia.sql
+++ Dzien5_baza_przychodniaZdrowia.sql
@@ -30,11 +30,13 @@ CREATE TABLE `godzWizytLekarza`(
 `Id_godzWizytLekarza` int(10) NOT NULL AUTO_INCREMENT,
 `Id_lekarz` int(10)  NOT NULL,
 `Id_pacjent` int(10) NOT NULL,
+`Id_godzPrzyjec`int(10) NOT NULL,
 `Dzien_Tygodnia` Date NOT NULL,
 `Godz_wizyty`TIME  NOT NULL,
 PRIMARY KEY(`Id_godzWizytLekarza`),
-FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (PersonID),
-FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(PersonID)) 
+FOREIGN KEY(`Id_godzPrzyjec`) REFERENCES godz_przyjec (Id_godzPrzyjec),
+FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (Id_pacjent),
+FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(Id_lekarz)) 
 DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `umowioneWizyty`;
@@ -44,8 +46,8 @@ CREATE TABLE `umowioneWizyty`(
 `Id_pacjent` int(10),
 `Date` Date NOT NULL ,
 PRIMARY KEY(`Id_wizyty`),
-FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (PersonID),
-FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(PersonID)) 
+FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (Id_pacjent),
+FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(Id_lekarz)) 
 DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `recepta`;
@@ -55,11 +57,35 @@ CREATE TABLE `recepta`(
 `Id_pacjent` int(10),
 `Nazwa_Leku` Text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL, 
 `Dawkowanie` Text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL, 
-FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (PersonID),
-FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(PersonID)
+FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (Id_pacjent),
+FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(Id_lekarz)
 )DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
+DROP TABLE IF EXISTS `godz_przyjec`;
+CREATE TABLE `godz_przyjec`(
+`Id_godzPrzyjec`int(10) NOT NULL AUTO_INCREMENT, 
+`Id_lekarz` int(10),
+`Id_pacjent` int(10),
+`Poniedziałek_Start` Time NOT NULL,
+`Poniedziałek_End` Time NOT NULL,
+`Wtorek_Start` Time NOT NULL,
+`Wtorek_End` Time NOT NULL,
+`Środa_Start` Time NOT NULL,
+`Środa_End` Time NOT NULL,
+`Czwartek_Start` Time NOT NULL,
+`Czwartek_End` Time NOT NULL,
+`Piatek_Start` Time NOT NULL,
+`Piatek_End` Time NOT NULL,
+FOREIGN KEY(`Id_pacjent`) REFERENCES pacjent (Id_pacjent),
+FOREIGN KEY(`Id_lekarz`) REFERENCES lekarz(Id_lekarz)
+)DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
+DROP TABLE IF EXISTS Activity;
+CREATE TABLE Activity(
+`Idactivity` int NOT NULL AUTO_INCREMENT,
+`Message` TEXT  CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+PRIMARY KEY(`Idactivity`))
+DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 
 
